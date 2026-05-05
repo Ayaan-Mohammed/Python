bc66ed02aeaf6dbf2086cb229122354e7358a417
Ayaan Mohammed <mohammed.ayaanyes@gmail.com>
2026-05-06 00:20:08 +0530

    add numpy basics

---
+++ numpybasics.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+list1=[0,1,2,3,4,5]
+list2=[2,4,6,8,10,12]
+arr1=np.array(list1)
+arr2=np.array(list2)
+
+# print(type(list1))
+# print(type(arr))
+
+# arofzero=np.zeros(5)
+# arofone=np.ones(5)
+
+# print(arofzero)
+# print(arofone)
+sum=arr1+arr2
+print("SUM: ", sum)
+mul=arr1*arr2
+print("PROD: ", mul)
+
