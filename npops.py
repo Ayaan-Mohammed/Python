d83346879f6aeb3cac7d6a56f6d68465d9711aa8
Ayaan Mohammed <mohammed.ayaanyes@gmail.com>
2026-05-06 00:25:28 +0530

    add numpy operations

---
+++ npops.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+list1=[0,2,4,5,6,7]
+arr1=np.array(list1)
+print("MAXIMUM: ", max(arr1))
+print("MINIMUM: ", min(arr1))
+print("MINIMUM: ", np.mean(arr1))
+print("MINIMUM: ", sum(arr1))
