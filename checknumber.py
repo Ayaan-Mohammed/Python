365d1de70ee515f4dc2d5f42d08cffbd39cc549f
Ayaan Mohammed <mohammed.ayaanyes@gmail.com>
2026-05-05 22:56:50 +0530

    add CheckNumber

---
+++ checknumber.py
@@ -0,0 +1,19 @@
+#To check if a number is: Even/Odd, Positive/Negative/Zero
+
+a= -23
+
+if(a>0) :
+    print("The number is Positive")
+
+elif (a==0):
+    print("The number is Zero")
+
+else:
+    print("The number is Negative")
+
+
+if(a%2==0):
+    print("Number is Even")
+
+else:
+    print("Number is Odd")
