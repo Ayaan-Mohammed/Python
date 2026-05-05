dbec7b56d231e11b810823424aba65ef595fe662
Ayaan Mohammed <mohammed.ayaanyes@gmail.com>
2026-05-06 00:09:20 +0530

    updated inputtasks

--- inputtasks.py
+++ inputtasks.py
@@ -1,12 +1,16 @@
-a=input("Enter 1st number: ")
-b=input("Enter 2nd number: ")
-c=input("Enter 3rd number: ")
+a=int (input("Enter 1st number: "))
+b=int(input("Enter 2nd number: "))
+c=int(input("Enter 3rd number: "))
 
-if (a>b) and (a>c):
+if ((a>b) and (a>c)):
     print(f"{a} is greater than {b} and {c}")
 
-elif (b>c) and (b>a):
+elif ((b>c) and (b>a)):
     print(f"{b} is greater than {a} and {c}")
 
 else:
     print(f"{c} is greater than {a} and {b}")
+
+print()
+str=str(input("Enter a String: "))
+print(len(str))
